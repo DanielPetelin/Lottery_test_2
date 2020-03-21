5c8b657558f2d5df0d39d71ceb485d1952c2f9a5
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-03-21 10:24:53 +0300

    lotery two

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 var num = prompt('Введите число от 1 до 200?');
+var myTxt = new Array(
+    "Да вы просто визунчик!",
+    "Вы победили!",
+    "Вы угадали!",
+    "Вам очень повезло!"
+);
+
+randnm = Math.round(Math.random() * (myTxt.length - 1));
 
 if (num == 18 || num == 111 || num == 183 || num == 50 || num == 15 || num == 7 || num == 190) {
-    alert('Вы просто визунчик!');
+    alert(myTxt[randnm]);
 } else if (num > 200) {
     alert('Вы ввели число больше 200!');
 } else if (num < 1) {
